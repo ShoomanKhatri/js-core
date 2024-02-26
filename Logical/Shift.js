f2193a38381fbeae7b6dd6ca2ecf000928543d56
Shooman Khatri <112597601+ShoomanKhatri@users.noreply.github.com>
2024-02-26 09:51:12 +0545

    Create Shift.js

---
+++ Logical/Shift.js
@@ -0,0 +1,9 @@
+
+let x = ["apple","banana","orange"];
+let y = x.shift();  
+//The shift() method in JavaScript removes the first element from an array and returns that removed element. 
+
+console.log(y);
+
+//output
+//apple
