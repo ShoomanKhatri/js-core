6c61c735cc9fa0db962fd11fc84215458dfbf97c
Shooman Khatri <112597601+ShoomanKhatri@users.noreply.github.com>
2024-02-29 12:40:24 +0545

    Create ArrayEqualityCheck.js

---
+++ Logical/ArrayEqualityCheck.js
@@ -0,0 +1,10 @@
+
+var arr1=[1,2,3]
+var arr2=[1,2,3]
+if(arr1==arr2)   //it compares references of the arrays not contents. It compares the memory locations.
+console.log("Same")
+else
+console.log(" Not same");
+
+//output
+//Not same
