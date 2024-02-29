9c979268d426f44ced454bdea44180165c991981
Shooman Khatri <112597601+ShoomanKhatri@users.noreply.github.com>
2024-02-29 14:16:59 +0545

    Create NaN.js

---
+++ Logical/NaN.js
@@ -0,0 +1,11 @@
+if(NaN===NaN){     //if there are == still it prints "Hi".
+    console.log("Hello");
+    }else{
+        console.log("Hi")
+    }
+
+
+//NaN is not equal to itself.
+//
+//Ouput
+//Hi
