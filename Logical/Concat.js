246e117bd967d5dc5c24d2d4618f333db885b47b
Shooman Khatri <112597601+ShoomanKhatri@users.noreply.github.com>
2024-03-01 07:48:33 +0545

    Concat.js

---
+++ Logical/Concat.js
@@ -0,0 +1,14 @@
+let s="hello";
+s.concat("there");  
+console.log(s);
+
+//output
+//hello
+//beacuse we havent assigned the concatenated value.
+
+//for concatenated output
+s=s.concat("there");
+console.log(s);
+
+//output
+//hellothere
