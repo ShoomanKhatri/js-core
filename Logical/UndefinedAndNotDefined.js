6b62626c5ef74acc93786d3f4cd3de7167ce9b81
Shooman Khatri <112597601+ShoomanKhatri@users.noreply.github.com>
2024-03-01 07:57:28 +0545

    Create UndefinedAndNotDefined.js

---
+++ Logical/UndefinedAndNotDefined.js
@@ -0,0 +1,10 @@
+var a;  //declaration
+console.log(a);      
+
+//if a variable is declared but not initialized, it  returns undefined.
+//output undefined
+
+console.log(b);  //not defined
+
+//output
+//not defined
