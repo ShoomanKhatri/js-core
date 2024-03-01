bffbc93ad421b93d946d3e6a58cbdc50fa7ffd35
Shooman Khatri <112597601+ShoomanKhatri@users.noreply.github.com>
2024-03-01 13:46:11 +0545

    Create NumPlusStr.js

---
+++ Logical/NumPlusStr.js
@@ -0,0 +1,12 @@
+let result=5+"5";
+console.log(result); //returns 55
+
+let a=5 + +"5";
+console.log(a);// returns 10
+
+let c=5-"5";
+console.log(c); //returns 0
+
+let d=5 - -"5";
+console.log(d);  //returns 10
+
